bf8107a52d55ffe75f0c7b9cf9e43271b70bb2df
ArtthSilva <arthur12pereira1@gmail.com>
2025-02-20 16:21:53 -0300

    add exercicios 2,3,4

--- src/components/Exercicio2.js
+++ src/components/Exercicio2.js
@@ -2,14 +2,23 @@ import { View, Text } from 'react-native'
 import React, { useState } from 'react'
 
 const Exercicio2 = () => {
-    const [count, setCount] = useState(0)
-    const increment = () => setCount(count + 1)
-    const decrement = () => setCount(count - 1)
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
   return (
-    <View>
-      <Text>Exercicio2</Text>
+    <View style={styles.container}>
+      <Button title="incrementar" onPress={increment} />
+      <Text>O contador está em: {contador}</Text>
+      <Button title="decrementar" onPress={decrement} />
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 100
+  },
+});
 
 export default Exercicio2
